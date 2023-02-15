20294579f0b8c945c6b05c8aa56daaa58d490111
Nilabja <goswamineel23@gmail.com>
2023-02-15 23:05:38 +0530

    my website

---
+++ script.js
@@ -0,0 +1,278 @@
+
+function hover1() {
+
+    const hover = document.getElementById("box1");
+    document.body.style.backgroundColor = "linear-gradient(rgb(22, 2, 2), black);";
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+function btnhover1() {
+    const hover = document.getElementById("btn_1");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '2px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhoverOut1() {
+    const hover = document.getElementById("btn_1");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black'; 
+}
+
+function hover2() {
+
+    const hover = document.getElementById("box2");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhover2() {
+    const hover = document.getElementById("btn_2");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '2px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhoverOut2() {
+    const hover = document.getElementById("btn_2");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black'; 
+}
+
+
+function hover3() {
+
+    const hover = document.getElementById("box3");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhover3() {
+    const hover = document.getElementById("btn_3");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '2px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhoverOut3() {
+    const hover = document.getElementById("btn_3");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black'; 
+}
+
+
+
+
+
+function hover4() {
+
+    const hover = document.getElementById("box4");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhover4() {
+    const hover = document.getElementById("btn_4");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '2px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhoverOut4() {
+    const hover = document.getElementById("btn_4");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black'; 
+}
+
+
+function hover5() {
+
+    const hover = document.getElementById("box5");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhover5() {
+    const hover = document.getElementById("btn_5");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '2px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhoverOut5() {
+    const hover = document.getElementById("btn_5");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black'; 
+}
+
+
+
+function hover6() {
+
+    const hover = document.getElementById("box6");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhover6() {
+    const hover = document.getElementById("btn_6");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '2px solid black';
+
+    hover.style.boxShadow = '1px 1px 15px 0px black';
+}
+
+function btnhoverOut6() {
+    const hover = document.getElementById("btn_6");
+
+
+    hover.style.zIndex = '60px';
+    hover.style.border = '1px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black'; 
+}
+
+function hoverOut1() {
+   
+    
+    const hover = document.getElementById("box1");
+
+
+    hover.style.zIndex = '0px';
+    hover.style.border = '0px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black';
+}
+
+function hoverOut2() {
+   
+    
+    const hover = document.getElementById("box2");
+
+
+    hover.style.zIndex = '0px';
+    hover.style.border = '0px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black';
+}
+
+function hoverOut3() {
+   
+    
+    const hover = document.getElementById("box3");
+
+
+    hover.style.zIndex = '0px';
+    hover.style.border = '0px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black';
+}
+
+function hoverOut4() {
+   
+    
+    const hover = document.getElementById("box4");
+
+
+    hover.style.zIndex = '0px';
+    hover.style.border = '0px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black';
+}
+
+function hoverOut5() {
+   
+    
+    const hover = document.getElementById("box5");
+
+
+    hover.style.zIndex = '0px';
+    hover.style.border = '0px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black';
+}
+
+function hoverOut6() {
+   
+    
+    const hover = document.getElementById("box6");
+
+
+    hover.style.zIndex = '0px';
+    hover.style.border = '0px solid black';
+
+    hover.style.boxShadow = '0px 0px 0px 0px black';
+}
+
+
+
+function glow() {
+    const glow = document.getElementById("Poster");
+    glow.style.boxShadow = '1px 0px 20px 0px  rgb(204, 248, 103)';
+}
+
+
+function dim() {
+    const glow = document.getElementById("Poster");
+    glow.style.boxShadow = '0px 0px 0px 0px  rgb(204, 248, 103)';
+}
